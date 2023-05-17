b8a90e13dafcdca23d9bcdfed294d1e56758cad7
JackBright1510 <gsalyrosasc1@upao.edu.pe>
2023-05-16 22:11:56 -0500

    Create CleanNowApplicationTests.java

---
+++ src/test/java/CleanNow01/CleanNow/CleanNowApplicationTests.java
@@ -0,0 +1,13 @@
+package CleanNow01.CleanNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
