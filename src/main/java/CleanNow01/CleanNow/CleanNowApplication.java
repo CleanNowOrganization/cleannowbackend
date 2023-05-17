ef2f69b2eb5d8938ae9276181e546b72f1791a4d
JackBright1510 <gsalyrosasc1@upao.edu.pe>
2023-05-16 22:11:43 -0500

    Create CleanNowApplication.java

---
+++ src/main/java/CleanNow01/CleanNow/CleanNowApplication.java
@@ -0,0 +1,13 @@
+package CleanNow01.CleanNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanNowApplication.class, args);
+	}
+
+}
